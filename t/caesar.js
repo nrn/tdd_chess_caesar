9aa9dedcde1157507c5063bfae151f4f4f7fdabf
Nick Niemeir <nick.niemeir@gmail.com>
2013-02-07 21:58:04 -0700

    Multiple chars

--- t/caesar.js
+++ t/caesar.js
@@ -1,9 +1,11 @@
 var test = require('tape')
   , caesar = require('../caesar.js')
+  , alph = 'abcdefghijklmnopqrstuvwxyz'
 
 test('caesar.js', function (t) {
   t.same(caesar(0, 'a'), 'a', 'rot0 returns same letter')
   t.same(caesar(1, 'a'), 'b', 'rot1 returns the next')
+  t.same(caesar(0, 'abc'), 'abc', 'handle multiple chars')
   t.end()
 })
 
